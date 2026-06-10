6864db1f69c1b3ab282e8c0e13112de9483edbd9
Avianz <kenneth.speecke@hotmail.com>
2026-06-10 13:59:12 +0200

    update project dependencies and SDK versions

--- frontend/obj/Debug/net10.0/frontend.AssemblyInfo.cs
+++ frontend/obj/Debug/net10.0/frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edc302248c3bba80e69e25104010816e3b0ba184")]
 [assembly: System.Reflection.AssemblyProductAttribute("frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
