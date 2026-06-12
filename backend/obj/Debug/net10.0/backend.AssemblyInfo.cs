d5b034d2c8477c6a44897bd68eedb502645588ff
Avianz <kenneth.speecke@hotmail.com>
2026-06-12 16:13:24 +0200

    update AssemblyInformationalVersion and related cache files

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e6591b9e9d648c2c18b08a98cffb1e101d9b96d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db74f1753af455908abcc09d4facca63eb47849d")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
